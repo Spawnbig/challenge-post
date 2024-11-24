cccfbd326230c597935d1ed953a6a354247f1bfd
Nicolas Sanhueza Soto <nsanhuezasoto97@gmail.com>
2024-11-24 00:43:04 -0300

    [dev] Feat: - Agregado docker-compose para entorno de prod

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,5 +1,7 @@
 import { DataSource } from "typeorm";
 import environment from "./environment";
+import * as path from "path";
+
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -10,6 +12,6 @@ export const AppDataSource = new DataSource({
   database: environment.DB_NAME,
   synchronize: true,
   logging: false,
-  entities: ["src/models/**/*.ts"],
-  migrations: ["src/migration/**/*.ts"],
+  entities: [path.join(__dirname, "../models/**/*.{js,ts}")],
+  migrations: [path.join(__dirname, "../migration/**/*.{js,ts}")],
 });
